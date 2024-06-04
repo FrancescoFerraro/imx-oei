a7c4341a000fa96455b29854568ce6e6abc87cfe
Alexander Herfurtner <alexander.h@variscite.com>
2024-06-04 18:44:09 +0200

    pinmux: add I2C1 pinumux settings
    This will add the pinmux settings for I2C1. Further, it moves it
    enables being pinmux always being part of the initialization.
    
    If DEBUG is not defined, it will still initialize I2C1 while not
    initializing UART.
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

--- include/pinmux.h
+++ include/pinmux.h
@@ -5,11 +5,5 @@
 #ifndef __PINMUX_H
 #define __PINMUX_H
 
-#ifdef DEBUG
 void pinmux_config(void);
-#else
-void pinmux_config(void)
-{
-}
-#endif
 #endif
--- source/utils/pinmux.c
+++ source/utils/pinmux.c
@@ -7,9 +7,15 @@
 #include <asm/types.h>
 #include "pinmux.h"
 
+#define IOMUXC_PAD_OD_MASK        0x800
+#define IOMUXC_PAD_OD_SHIFT       11
+#define IOMUXC_PAD_OD(x)          (((u32)(((u32)(x)) << IOMUXC_PAD_OD_SHIFT)) & IOMUXC_PAD_OD_MASK)
 #define IOMUXC_PAD_PD_MASK        0x400
 #define IOMUXC_PAD_PD_SHIFT       10
 #define IOMUXC_PAD_PD(x)          (((u32)(((u32)(x)) << IOMUXC_PAD_PD_SHIFT)) & IOMUXC_PAD_PD_MASK)
+#define IOMUXC_PAD_PU_MASK        0x200
+#define IOMUXC_PAD_PU_SHIFT       9
+#define IOMUXC_PAD_PU(x)          (((u32)(((u32)(x)) << IOMUXC_PAD_PU_SHIFT)) & IOMUXC_PAD_PU_MASK)
 #define IOMUXC_PAD_DSE_MASK       0x7E
 #define IOMUXC_PAD_DSE_SHIFT      1
 #define IOMUXC_PAD_DSE(x)         (((u32)(((u32)(x)) << IOMUXC_PAD_DSE_SHIFT)) & IOMUXC_PAD_DSE_MASK)
@@ -19,6 +25,9 @@
 #define IOMUXC_PAD_SION_MASK      0x10
 #define IOMUXC_PAD_SION_SHIFT     4
 #define IOMUXC_PAD_SION(x)        (((u32)(((u32)(x)) << IOMUXC_PAD_SION_SHIFT)) & IOMUXC_PAD_SION_MASK)
+#define IOMUXC_PAD_FSEL1_MASK     0x180
+#define IOMUXC_PAD_FSEL1_SHIFT    7
+#define IOMUXC_PAD_FSEL1(x)       (((u32)(((u32)(x)) << IOMUXC_PAD_FSEL1_SHIFT)) & IOMUXC_PAD_FSEL1_MASK)
 
 #define IOMUXC_BASE	IOMUXC_RBASE
 #define IOR(x)		(IOMUXC_BASE + x)
@@ -31,7 +40,10 @@ static const struct pinmux_cfg {
 	u32 inOnFld;
 	u32 confReg;
 	u32 confVal;
-} lpuart_pinmux_cfg[] = {
+} pinmux_cfg[] = {
+	/* LPI2C1 */
+	{ IOR(0x1C0), 0, IOR(0), 0, 1U, IOR(0x3C4), IOMUXC_PAD_DSE(0xf) | IOMUXC_PAD_FSEL1(3) | IOMUXC_PAD_PU(1) | IOMUXC_PAD_OD(1) },
+	{ IOR(0x1C4), 0, IOR(0), 0, 1U, IOR(0x3C8), IOMUXC_PAD_DSE(0xf) | IOMUXC_PAD_FSEL1(3) | IOMUXC_PAD_PU(1) | IOMUXC_PAD_OD(1) },
 #ifdef DEBUG
 #if (DEBUG_UART_INSTANCE == 1)
 	{ IOR(0x1D0), 0, IOR(0), 0, 0, IOR(0x3D4), IOMUXC_PAD_PD(1) },
@@ -49,7 +61,7 @@ void pinmux_config(void)
 	const struct pinmux_cfg *cfg;
 	u32 muxVal;
 
-	for (cfg = lpuart_pinmux_cfg; cfg->muxReg != 0; cfg++) {
+	for (cfg = pinmux_cfg; cfg->muxReg != 0; cfg++) {
 		muxVal = IOMUXC_PAD_MUX_MODE(cfg->muxMode) | IOMUXC_PAD_SION(cfg->inOnFld);
 		writel(muxVal, cfg->muxReg);
 		writel(cfg->inDaisy, cfg->inReg);
